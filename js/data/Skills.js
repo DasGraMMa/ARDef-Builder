e14906871be12c9ad9bb9702e00526cf15e49ca1
GraMMa <marcel@gramma.name>
2019-01-08 23:52:29 +0100

    Add more base for the Skills container
    + added getSkill method for a single skill
    + added getAllSkillsOf for a given type
    + added getAllSkills for the plain data
    + added complicated typedefs

--- js/data/Skills.js
+++ js/data/Skills.js
@@ -40,13 +40,36 @@ import { Gr } from "../GrUtils.js";
  */
 
 /**
+ * @typedef {Object} SkillGeneral
+ * @property {String} name the name of the skill
+ * @property {Number} spCost the SP cost of this skill
+ * @property {String} icon the icon indices of this skill (format: "row-column")
+ * @property {Array.<String>} [prev] the skill names of the required skills for this one to be learned
+ * @property {Array.<SkillRestrictor>} [include] the filter for Heroes which are allowed to use this skill
+ * @property {Array.<SkillRestrictor>} [exclude] the filter for Heroes which are not allowed to use this skill
+ * @property {SkillStatChange} [stats] the stat change this skill makes when equipped
+ * @property {boolean} [last] whether this skill is the last in its sequence
+ * @property {String} effect the effect this assist has
+ */
+
+/**
+ * @typedef {Object} SkillSeal
+ * @property {String} name the name of the skill
+ * @property {String} icon the icon indices of this skill (format: "row-column")
+ * @property {Array.<SkillRestrictor>} [include] the filter for Heroes which are allowed to equip this seal
+ * @property {Array.<SkillRestrictor>} [exclude] the filter for Heroes which are not allowed to equip this seal
+ * @property {SkillStatChange} [stats] the stat change this skill makes when equipped
+ * @property {String} effect the effect this assist has
+ */
+
+/**
  * @typedef {Object} SkillHolder
  * @property {Array.<SkillWeapon>} weapons the list of weapon skills
  * @property {Array.<SkillAssist>} assists the list of assist skills
  * @property {Array.<SkillSpecial>} specials the list of special skills
- * @property {Array.<SkillA>} aSkills the list of A skills
- * @property {Array.<SkillB>} bSkills the list of B skills
- * @property {Array.<SkillC>} cSkills the list of C skills
+ * @property {Array.<SkillGeneral>} aSkills the list of A skills
+ * @property {Array.<SkillGeneral>} bSkills the list of B skills
+ * @property {Array.<SkillGeneral>} cSkills the list of C skills
  * @property {Array.<SkillSeal>} seals the list of seals of skills
  */
 
@@ -63,6 +86,41 @@ class Skills
          * @type {SkillHolder}
          */
         this.data = initialData;
+    }
+
+    /**
+     * Returns the (first found) skill with the given name of the given type.
+     * 
+     * @param {String} skillName the name of the skill to search for
+     * @param {String} type the type of the skill
+     * @returns the requested skill or undefined, if it couldn't be found
+     */
+    getSkill(skillName, type)
+    {
+        let skillsToCheck = this.getAllSkillsOf(type);
+
+        return skillsToCheck.find(curSkill => curSkill.name == skillName);
+    }
+
+    /**
+     * Returns the container with the skills of the given type.
+     * 
+     * @param {String} type the name of the type to get the skills for
+     * @returns {Array} the container with the skills of the type
+     */
+    getAllSkillsOf(type)
+    {
+        return this.data[type];
+    }
+
+    /**
+     * Returns the plain underlying Skills data.
+     * 
+     * @returns {SkillHolder} the underlying data
+     */
+    getAllSkills()
+    {
+        return this.data;
     }
 
     static async loadSkillData()
@@ -89,5 +147,7 @@ class Skills
         return toReturn;
     }
 }
+
+Skills.EMPTY = Object.freeze({ name: "-", icon: "", effect: "" });
 
 export { Skills };
